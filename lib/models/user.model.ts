fe4b5f12085331903d5c4abe30d96cd53008234c
Jamal Nafea <jamal.s.nafea@gmail.com>
2024-02-09 19:48:58 +0200

    first

---
+++ lib/models/user.model.ts
@@ -0,0 +1,39 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+  id: {
+    type: String,
+    required: true,
+  },
+  username: {
+    type: String,
+    unique: true,
+    required: true,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  image: String,
+  bio: String,
+  threads: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Thread", // One user can have multiple threads
+    },
+  ],
+  onboarded: {
+    type: Boolean,
+    default: false,
+  },
+  communities: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Community", // this community instance that stored on database
+    },
+  ],
+});
+
+const User = mongoose.models.User || mongoose.model("User", userSchema);
+
+export default User;
