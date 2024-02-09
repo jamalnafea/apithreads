2ae016e4b7cb6a6d22b8b560e5693819c96b4139
Jamal Nafea <jamal.s.nafea@gmail.com>
2024-02-09 20:04:08 +0200

    second

---
+++ lib/models/thread.model.ts
@@ -0,0 +1,36 @@
+import mongoose from "mongoose";
+
+const threadSchema = new mongoose.Schema({
+  text: {
+    type: String,
+    required: true,
+  },
+  author: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  community: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Community",
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  parentId: {
+    // this is schema for comments on the original thread
+    type: String,
+  },
+  children: [
+    {
+      // this is schema for reply on comment anther on comment
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Thread",
+    },
+  ],
+});
+
+const Thread = mongoose.models.Thread || mongoose.model("Thread", threadSchema);
+
+export default Thread;
